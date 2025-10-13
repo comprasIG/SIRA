9b8e7b2ae260b83630d57017f6cb08f7493954d5
comprasIG <202018607+comprasIG@users.noreply.github.com>
2025-10-13 16:04:09 -0600

    feat: scaffold para la funcionalidad 'Pick In'

--- sira-front/src/App.jsx
+++ sira-front/src/App.jsx
@@ -1,3 +1,4 @@
+import PickInPage from "./pages/PickInPage";
 //C:\SIRA\sira-front\src\App.jsx
 // src/App.jsx
 import React from "react";
@@ -71,7 +72,8 @@ console.log(import.meta.env.VITE_API_URL); // Verifica que la variable de entorn
           <Route path="/agregar-producto" element={<RutaProtegida permiso="AGREGAR_PRODUCTO"><MainLayout><AgregarProducto /></MainLayout></RutaProtegida>} />
           <Route path="/lista-producto" element={<RutaProtegida permiso="AGREGAR_PRODUCTO"><MainLayout><ListaProductos /></MainLayout></RutaProtegida>}/>
           <Route  path="/config/notificaciones" element={<RutaProtegida permiso="SUPERUSUARIO"><MainLayout> <NotificacionesPage /></MainLayout></RutaProtegida>}/>
-         {/* --- AÑADIR NUEVAS RUTAS AUTOMÁTICAMENTE AQUÍ --- */}
+                   <Route path="PICK_IN" element={<RutaProtegida permiso="PICK_IN"><MainLayout><PickInPage /></MainLayout></RutaProtegida>}/>
+          {/* --- AÑADIR NUEVAS RUTAS AUTOMÁTICAMENTE AQUÍ --- */}
          
           {/* Redirección y página no encontrada */}
           <Route path="/" element={<Navigate to={usuario ? "/dashboard" : "/login"} replace />} />
