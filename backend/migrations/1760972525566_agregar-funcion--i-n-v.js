9c2e1e91ac2b9b5b29c9166dadb20f28d9dce857
comprasIG <202018607+comprasIG@users.noreply.github.com>
2025-10-20 09:02:07 -0600

    feat: scaffold para la funcionalidad 'Inventario'

---
+++ backend/migrations/1760972525566_agregar-funcion--i-n-v.js
@@ -0,0 +1,19 @@
+
+/** @type {import('node-pg-migrate').ColumnDefinitions | undefined} */
+export const shorthands = undefined;
+/** @param pgm {import('node-pg-migrate').MigrationBuilder} */
+export const up = async (pgm) => {
+  await pgm.sql(`
+    INSERT INTO public.funciones (codigo, nombre, modulo, icono, ruta)
+    VALUES ('INV', 'Inventario', 'Almacén', 'Warehouse', '/INV')
+    ON CONFLICT (codigo) DO UPDATE SET
+      nombre = EXCLUDED.nombre,
+      modulo = EXCLUDED.modulo,
+      icono = EXCLUDED.icono,
+      ruta = EXCLUDED.ruta;
+  `);
+};
+/** @param pgm {import('node-pg-migrate').MigrationBuilder} */
+export const down = async (pgm) => {
+  await pgm.sql(`DELETE FROM public.funciones WHERE codigo = 'INV';`);
+};
