7bab66cf6f79c8e1e8b27e9079d4c0f0867cfbda
comprasIG <202018607+comprasIG@users.noreply.github.com>
2025-12-01 14:05:17 -0600

    feat: scaffold para la funcionalidad 'Ver Empleados'

---
+++ sira-front/src/pages/VerEmpleadosPage.jsx
@@ -0,0 +1,12 @@
+// src/pages/VerEmpleadosPage.jsx
+import React from 'react';
+import VerEmpleados from '../components/-r.-h./VerEmpleados';
+
+export default function VerEmpleadosPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Módulo: Ver Empleados</h1>
+      <VerEmpleados />
+    </div>
+  );
+}
