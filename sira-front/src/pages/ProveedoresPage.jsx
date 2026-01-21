40869084c7158f08ee46eb18bef05d632b9e501c
comprasIG <202018607+comprasIG@users.noreply.github.com>
2026-01-21 16:37:55 -0600

    feat: scaffold para la funcionalidad 'Proveedores'

---
+++ sira-front/src/pages/ProveedoresPage.jsx
@@ -0,0 +1,12 @@
+// src/pages/ProveedoresPage.jsx
+import React from 'react';
+import Proveedores from '../components/-compras/Proveedores';
+
+export default function ProveedoresPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Módulo: Proveedores</h1>
+      <Proveedores />
+    </div>
+  );
+}
