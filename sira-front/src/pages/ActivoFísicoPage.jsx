ef66bd446a2697ce5e0834b0c049b091db795751
comprasIG <202018607+comprasIG@users.noreply.github.com>
2026-03-04 11:23:14 -0600

    feat: scaffold para la funcionalidad 'Activo Físico'

---
+++ sira-front/src/pages/ActivoFísicoPage.jsx
@@ -0,0 +1,12 @@
+// src/pages/ActivoFísicoPage.jsx
+import React from 'react';
+import ActivoFísico from '../components/-almacén/ActivoFísico';
+
+export default function ActivoFísicoPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Módulo: Activo Físico</h1>
+      <ActivoFísico />
+    </div>
+  );
+}
